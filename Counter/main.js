7a2ba51a291b7318070fd51fa1fd4b2f937c5199
Vineeth Wilson <vineethwilson15@gmail.com>
2021-08-16 22:40:31 +0530

    DAY 3

---
+++ Counter/main.js
@@ -0,0 +1,18 @@
+let a=0
+
+function add()
+{
+    a++
+    document.getElementById('screen').innerText=a
+}
+
+function sub()
+{
+    a--
+    document.getElementById('screen').innerText=a
+}
+function reset()
+{
+    a=0
+    document.getElementById('screen').innerText=a
+}
