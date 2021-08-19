616300383a3d0da06ef3fcd826611651399f6f9e
Vineeth Wilson <vineethwilson15@gmail.com>
2021-08-19 14:47:22 +0530

    DAY 5

---
+++ Counter-Project/main.js
@@ -0,0 +1,10 @@
+function increment(){
+    let val=document.getElementById("num").innerHTML;
+    let num=Number(val)+1
+    document.getElementById("num").innerHTML=num;
+}
+function decrement(){
+    let val=document.getElementById("num").innerHTML;
+    let num=Number(val)-1
+    document.getElementById("num").innerHTML=num;
+}
